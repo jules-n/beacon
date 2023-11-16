90a435ca63a1a80bf88668597b428b9cfa8e3a8b
jules-n <bonkers.superhero@gmail.com>
2023-11-16 02:30:45 +0200

    Euruka server setup

---
+++ src/test/java/year/exp/beacon/BeaconApplicationTests.java
@@ -0,0 +1,13 @@
+package year.exp.beacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeaconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
